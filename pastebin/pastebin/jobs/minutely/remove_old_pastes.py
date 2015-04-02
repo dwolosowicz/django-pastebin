39e8e606e6c844c7bc774e541f1246927679b7ef
Dawid Wołosowicz <dawid.wolosowicz@polcode.net>
2015-04-02 09:02:42 +0200

    Refactored remove_old_pastes query to be more Django

--- pastebin/pastebin/jobs/minutely/remove_old_pastes.py
+++ pastebin/pastebin/jobs/minutely/remove_old_pastes.py
@@ -1,11 +1,9 @@
 from django_extensions.management.jobs import MinutelyJob
-from django.db import connection
+from django.db.models import Q
 from pastebin.models import Paste
 
 class Job(MinutelyJob):
     help = "Job removes Pastes that have expired"
 
     def execute(self):
-        query = "DELETE FROM {} WHERE NOW() > DATE_ADD(created, INTERVAL expires_in SECOND) AND expires_in > 0".format(Paste._meta.db_table)
-
-        connection.cursor().execute(query)
+        Paste.objects.filter(expires_in__gt=0).extra(where=["NOW() > DATE_ADD(created, INTERVAL expires_in SECOND)"]).delete()
